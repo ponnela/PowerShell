14ae08d44315e9e0f4e87d1bf29811a59cab705a
ponnela <36950605+ponnela@users.noreply.github.com>
2019-11-21 11:15:01 +0200

    Create SyncAzureGroupToADGroup.ps1

---
+++ SyncAzureADGroupToOnPremAD/SyncAzureGroupToADGroup.ps1
@@ -0,0 +1,48 @@
+﻿Import-Module AzureAD
+import-module ActiveDirectory
+Connect-AzureAD -Credential
+
+$AZPrefix= "AZ" # Jos halutaan azure-ryhmän nimestä poistaa jotain ennen OnPremryhmän nimen muodostamista
+$ADPrefix= "AD" # Jos halutaan ad-ryhmän nimeen lisätä jotain ennen OnPremryhmän nimen muodostamista
+
+$azgroupstosyncgroup=Get-AzureADGroup -SearchString "AZGroupsToSync" # Luetaan tästä Azure-ryhmästä synkronoitavat ryhmät
+
+$GroupsToSync=Get-AzureADGroupMember -ObjectId $azgroupstosyncgroup.ObjectId -All $true # Haetaan synronoitavat ryhmät
+
+foreach ($group in $GroupsToSync)
+   {
+   $AzureGroupMembers=Get-AzureADGroupMember -ObjectId $group.ObjectId -All $true 
+   $ADGroupName=$group.DisplayName -replace $AZPrefix , $ADPrefix
+   $ADGroupMembers=Get-ADGroupMember -Identity $ADGroupName
+   foreach ($user in $AzureGroupMembers)
+      {
+      if ($ADGroupMembers.distinguishedName -ccontains $user.ExtensionProperty.onPremisesDistinguishedName) 
+         {
+         # Jos käyttäjä oli jo ryhmässä jäsenenä 
+         if ($ADGroupMembers.Count)
+            {
+            $ADGroupMembers = $ADGroupMembers | ? {$_.distinguishedName -ne $user.ExtensionProperty.onPremisesDistinguishedName}
+            }
+         else
+            {
+            $ADGroupMembers = ""
+            }
+         }
+      else
+         {
+          # Lisätään uusi käyttäjä ryhmään
+         Add-ADGroupMember -Identity $ADGroupName -Members $user.ExtensionProperty.onPremisesDistinguishedName
+         }
+      }
+   if ($ADGroupMembers)
+      {
+      # Poistetaan ryhmästä jäsenet joita ei ole Azuren ryhmän jäseninä
+      foreach ($aduser in $ADGroupMembers)
+         {
+         Remove-ADGroupMember -Identity $ADGroupName -Members $aduser.distinguishedName -Confirm:$false
+         }
+      }
+   }
+
+
+
